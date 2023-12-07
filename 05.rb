431d8123cbfe158c0a481fb0c695e68460bb1b8a
viizhu <vii.zhu@gmail.com>
2023-12-06 23:06:29 -0500

    Day 5 Part 2 (wip)

--- 05.rb
+++ 05.rb
@@ -1,9 +1,10 @@
 class Puzzle5
-  attr_accessor :input, :seeds, :almanac, :location
+  attr_accessor :input, :seeds, :part_2_seeds, :almanac, :location
 
   def initialize
     @input = File.readlines('05-input.txt')
     @seeds = []
+    @part_2_seeds = []
     @almanac = []
     @location = []
 
@@ -26,29 +27,52 @@ class Puzzle5
       end
     end
 
+    @seeds.each_slice(2) { |s| @part_2_seeds << s }
     puts "seeds: #{seeds}"
     puts "almanac: #{almanac}"
+    puts "part_2_seeds: #{part_2_seeds}"
   end
 
   def source_in_destination?(source, map)
     map.each_with_index.map do |m, i|
-      return i if (source >= m[1] && source <= (m[1] + m[2]))
+      # puts "map: #{map}, m: #{m}, i: #{i}"
+      # print "\nif #{source[0]} "
+      # print "<= #{(m[1] + m[2])} "
+      # print "&& #{m[1]} "
+      # print "<= #{(source[0] + source[1])}\n"
+      if (source[0] <= (m[1] + m[2])) && (m[1] <= (source[0] + source[1]))
+        # puts "i: #{i}"
+        # puts "map[i]: #{map[i]}"
+        return i
+      # elsif (source[0] <= (m[1] + m[2])) && !(m[1] <= (source[0] + source[1]))
+      #   # puts "hi"
+      #   return nil
+      # elsif !(source[0] <= (m[1] + m[2])) && (m[1] <= (source[0] + source[1]))
+      #   # puts "bye"
+      #   return nil
+      # else
+        # puts "else"
+        # return nil
+      end
     end
     nil
   end
 
   def set_destination(source, i, map)
-    source - map[i][1] + map[i][0]
+    puts "-----------source: #{source}, i: #{i}, map: #{map}"
+    puts "map[i]: #{map[i]}"
+    79
+    # source - map[i][1] + map[i][0]
   end
 
   def lowest_location_number
-    seeds.each do |seed|
-      source = seed
+    part_2_seeds.each do |seed_range|
+      source = seed_range
       almanac.each do |map|
         if source_in_destination?(source, map)
           source = set_destination(source, source_in_destination?(source, map), map)
-        else
-          source = source
+        # else
+        #   source = source
         end
       end
       location << source
